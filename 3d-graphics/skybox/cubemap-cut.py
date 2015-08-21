dc53297345ae49696c8e406a6fd017056454a042
Danke Xie <d.xie@samsung.com>
2015-08-21 11:13:32 -0700

    update cubemap-cut

--- 3d-graphics/skybox/cubemap-cut.py
+++ 3d-graphics/skybox/cubemap-cut.py
@@ -2,22 +2,38 @@
 
 from __future__ import print_function
 import sys
+import os.path
 from PIL import Image
 
+if len(sys.argv) < 2:
+    print("Usage: cubemap-cut.py <filename.jpg|png>")
+    sys.exit(-1)
+
 infile = sys.argv[1]
+filename, file_extension = os.path.splitext(infile)
 
-name_map = \
-    ["", "", "top", "",
-     "back", "left", "front", "right",
-     "", "", "bottom", ""]
-
-print(infile)
+name_map = [ \
+     ["", "", "posy", ""],
+     ["posz", "negx", "negz", "posx"],
+     ["", "", "negy", ""]]
 
 try:
     im = Image.open(infile)
     print(infile, im.format, "%dx%d" % im.size, im.mode)
-    
-    print(name_map);
+
+    width, height = im.size
+
+    cube_size = width / 6
+
+    print("%dx%d cube %d" % (width, height, cube_size))
+    for row in range(3):
+        for col in range(4):
+            if name_map[row][col] != "":
+                sx = cube_size * col
+                sy = cube_size * row
+                fn = name_map[row][col] + file_extension
+                print("%s --> %s" % (str((sx, sy, sx + cube_size, sy + cube_size)), fn))
+                im.crop((sx, sy, sx + cube_size, sy + cube_size)).save(fn) 
 
 except IOError:
     pass
