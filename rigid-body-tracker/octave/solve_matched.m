201c089e6bd4f0cdb3f872b87f29c375b2aefe7f
Danke Xie <d.xie@samsung.com>
2017-08-30 18:58:22 -0700

    add solvers

---
+++ rigid-body-tracker/octave/solve_matched.m
@@ -0,0 +1,6 @@
+% Solve the transform on the object knowing projection of each point
+% X is the original 3D coordinates
+% Xb is observed 3D coordinates
+% P is the camera matrix
+function T = solve_matched(X, Xb, P)
+endfunction
---
+++ rigid-body-tracker/octave/solve_unmatched.m
@@ -0,0 +1,6 @@
+% Solve the transform on the object knowing projection of each point
+% X is the original 3D coordinates
+% Xp 3D coordinates of unmatched point observations, |Xp| <= |X|
+% P is the camera matrix
+function T = solve_unmatched(X, Xp, P)
+endfunction
